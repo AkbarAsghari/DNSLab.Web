180c71f2fb3402b6733eb010e4df50b95105268e
Akbar <mrakbarasghari@gmail.com>
2026-05-05 19:55:37 +0330

    Add Social Channel

--- DNSLab.Web/Helpers/AllRoutes.cs
+++ DNSLab.Web/Helpers/AllRoutes.cs
@@ -69,5 +69,7 @@
         public const string UdpReverseProxy = "/reverse-proxy/udp";
         public const string HttpReverseProxy = "/reverse-proxy/http";
         public const string ReverseProxyTraffic = "/reverse-proxy/traffic";
+
+        public const string SocialChannel = "https://ble.ir/dnslab";
     }
 }

4992fe74434a61b69c05f7b436827f727c98ae0c
Akbar <mrakbarasghari@gmail.com>
2026-07-09 08:25:44 +0330

    Add Quick Access

--- DNSLab.Web/Components/Pages/Defaults/Dashboard.razor.cs
+++ DNSLab.Web/Components/Pages/Defaults/Dashboard.razor.cs
@@ -7,18 +7,18 @@ namespace DNSLab.Web.Components.Pages.Defaults;
 partial class Dashboard
 {
     [Inject] IStaticRepository _StaticRepository { get; set; }
-    [Inject] IReverseProxyTrafficRepository _ReverseProxyTrafficRepository { get; set; }
+    [Inject] NavigationManager _NavigationManager { get; set; }
 
     int? ZoneCount { get; set; }
     int? RecordsCount { get; set; }
     int? DDNSsCount { get; set; }
-    IEnumerable<ReverseProxyTrafficDTO>? _Traffics { get; set; }
+    int? OnlineTunnelsCount { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         ZoneCount = await _StaticRepository.GetZonesCount();
         RecordsCount = await _StaticRepository.GetRecordsCount();
         DDNSsCount = await _StaticRepository.GetDDNSsCount();
-        _Traffics = await _ReverseProxyTrafficRepository.GetCurrentTraffics();
+        OnlineTunnelsCount = await _StaticRepository.OnlineTunnelsCount();
     }
 }
